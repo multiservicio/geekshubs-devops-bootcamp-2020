911476c8f8fb69440652dcfcdef636a0f3545c25
Pedro Diaz <pedro.diaz@kiosked.com>
2020-01-20 07:19:12 +0100

    WIP

---
+++ scripts/provisioning/microk8s.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "Installing microk8s via snap"
+sudo snap install microk8s --classic
+
+echo "Checking the status"
+sudo microk8s.status --wait-ready
+
+echo "Turning on standard services"
+sudo microk8s.enable dns dashboard registry
